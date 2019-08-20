f772a0341a9f3ceeace70e5f25735d8a83bd682c
Ishwar Gurung <ishwar.gurung@dazn.com>
2019-08-20 15:14:31 +0100

    modify s3

--- s3.tf
+++ s3.tf
@@ -1,4 +1,10 @@
 resource "aws_s3_bucket" "testbucket" {
-  bucket = "mytf-testishwar-bucket"
-  acl = "private"
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+  versioning_enabled = "true"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }

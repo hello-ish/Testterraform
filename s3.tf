086734ed9896b6b0f161e75c796ca785713cd683
Ishwar Gurung <ishwar.gurung@dazn.com>
2019-08-20 15:23:37 +0100

    s3 versioning

--- s3.tf
+++ s3.tf
@@ -1,10 +1,7 @@
 resource "aws_s3_bucket" "testbucket" {
   bucket = "my-tf-testishwar-bucket"
   acl    = "private"
-  versioning {
-        enabled = true
-  }
-
+  
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
